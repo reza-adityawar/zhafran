915bfe845db6efa61ae40bd372df286a4fca6287
reza <muhamadreza34@gmail.com>
2026-08-17 04:27:30 +0700

    chore: expose deploy revision in healthcheck

--- app/routes/healthcheck.jsx
+++ app/routes/healthcheck.jsx
@@ -1,3 +1,4 @@
 import { json } from "@remix-run/node";
 
-export const loader = () => json({ ok: true });
+export const loader = () =>
+  json({ ok: true, revision: process.env.VERCEL_GIT_COMMIT_SHA || "unknown" });
